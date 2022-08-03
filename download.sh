568bc6d8849ebed633f4e1e037567e092dbb6392
l0r3m1psum <bellanidiego@gmail.com>
2022-08-03 18:23:58 +0200

    Fixed typo in curl options.

--- download.sh
+++ download.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-curl -Zi --output-dir data \
+curl -Z --output-dir data \
 	-sO 'https://pjreddie.com/media/files/mnist_train.csv' \
 	-sO 'https://pjreddie.com/media/files/mnist_test.csv'
